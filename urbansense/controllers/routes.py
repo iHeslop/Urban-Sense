6727e659657f760af228802482ce5858b42cbd30
iHeslop <iheslop182@gmail.com>
2024-07-01 13:34:04 +1000

    Added traffic data route

--- urbansense/controllers/routes.py
+++ urbansense/controllers/routes.py
@@ -3,6 +3,7 @@ from urbansense.services.traffic_data import populate_traffic_data
 from urbansense.data.traffic_analysis import run_spark_job
 from urbansense.db_config import db
 from urbansense.models.traffic_analysis_model import TrafficAnalysisResult
+from urbansense.models.traffic_data_model import CityTrafficData
 
 routes_bp = Blueprint('routes', __name__)
 
@@ -42,5 +43,29 @@ def get_all_results():
         speed_ratios = [
             {"city": result.city, "speed_ratio": result.speed_ratio, "timestamp": result.timestamp} for result in results]
         return jsonify(speed_ratios), 200
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
+
+
+@routes_bp.route('/city-traffic-data', methods=['GET'])
+def get_all_city_traffic_data():
+    try:
+        results = CityTrafficData.query.all()
+
+        result_data = [
+            {
+                "id": result.id,
+                "city": result.city,
+                "location": result.location,
+                "timestamp": result.timestamp,
+                "current_travel_time": result.current_travel_time,
+                "free_flow_travel_time": result.free_flow_travel_time,
+                "free_flow_speed": result.free_flow_speed,
+                "current_speed": result.current_speed
+            }
+            for result in results
+        ]
+
+        return jsonify(result_data), 200
     except Exception as e:
         return jsonify({"message": str(e)}), 500
