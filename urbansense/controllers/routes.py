22f4d7e1d516593e54cf7fc18b6dade7972ad208
iHeslop <iheslop182@gmail.com>
2024-07-01 10:33:30 +1000

    virtual environment setup

--- urbansense/controllers/routes.py
+++ urbansense/controllers/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 from urbansense.services.traffic_data import populate_traffic_data
+from urbansense.data.traffic_analysis import run_spark_job
 
 routes_bp = Blueprint('routes', __name__)
 
@@ -9,7 +10,16 @@ def hello():
     return "Hello World!"
 
 
-@routes_bp.route("/populate_traffic_data")
+@routes_bp.route("/populate-traffic-data")
 def populate_traffic_data_route():
     populate_traffic_data()
     return jsonify({'message': 'Traffic data populated.'}), 200
+
+
+@routes_bp.route('/run-spark-job', methods=['GET'])
+def trigger_spark_job():
+    try:
+        run_spark_job()
+        return jsonify({"message": "Spark job completed successfully."}), 200
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
---
+++ urbansense/data/traffic_analysis.py
@@ -0,0 +1,64 @@
+import os
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import avg, col
+from urbansense.models.traffic_analysis_model import TrafficAnalysisResult
+from urbansense.db_config import db
+from dotenv import load_dotenv
+
+
+def run_spark_job():
+    try:
+        load_dotenv()
+        DB_NAME = os.getenv('DB_NAME')
+        DB_PASSWORD = os.getenv('DB_PASSWORD')
+        DB_USER = os.getenv('DB_USER')
+        DB_HOST = os.getenv('DB_HOST')
+        DB_PORT = os.getenv('DB_PORT')
+
+        spark = SparkSession.builder \
+            .appName("Traffic Analysis") \
+            .config("spark.jars", "/path/to/mysql-connector-java.jar") \
+            .getOrCreate()
+
+        jdbc_url = f"jdbc:mysql://{DB_HOST}:{DB_PORT}/{DB_NAME}"
+        table_name = "city_traffic_data"
+
+        df = spark.read \
+            .format("jdbc") \
+            .option("url", jdbc_url) \
+            .option("dbtable", table_name) \
+            .option("user", DB_USER) \
+            .option("password", DB_PASSWORD) \
+            .option("driver", "com.mysql.cj.jdbc.Driver") \
+            .load()
+
+        avg_speed_per_city = df.groupBy("city") \
+            .agg(
+                avg("current_speed").alias("avg_current_speed"),
+                avg("free_flow_speed").alias("avg_free_flow_speed")
+        )
+
+        avg_speed_per_city = avg_speed_per_city.withColumn(
+            "speed_ratio", col("avg_current_speed") /
+
+            col("avg_free_flow_speed")
+        )
+
+        rows = avg_speed_per_city.collect()
+
+        for row in rows:
+            analysis_entry = TrafficAnalysisResult(
+                city=row['city'],
+                avg_current_speed=row['avg_current_speed'],
+                avg_free_flow_speed=row['avg_free_flow_speed'],
+                speed_ratio=row['speed_ratio']
+            )
+            db.session.add(analysis_entry)
+            db.session.commit()
+
+        spark.stop()
+        return "Spark job completed successfully."
+
+    except Exception as e:
+        return str(e)
